f48602f7589eef1e5bc7805ad61b76ec756451f4
Shohid <shukurovshohid7@gmail.com>
2022-11-26 19:16:19 +0500

    code wars

---
+++ index.js
@@ -0,0 +1,35 @@
+// let acc = {
+//   name: "shohid",
+//   major: "student",
+// };
+
+// for (let key in acc) {
+//   console.log(acc);
+// }
+
+// function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return
+//       (results);
+
+// var greet_abe = function() {
+//     var name = 'Abe';
+//       return "Hello, " + name + '!';
+//     };
+//     var greet_ben = function() {
+//     name = 'Ben';
+//       return "Hello, " + name + '!';
+//     };
+
+
+// name = 'The Window';
+
+// var alpha = {
+//     name : 'My Alpha',
+//     getNameFunc : function() {
+//         return function() {
+//             return alpha.name;
+//         };
+//     }
+// };
